0ac39178dc1de02adf46d83daaf6d0b0d11c2e78
pedsarg <pedsar@gmail.com>
2026-01-31 17:08:13 -0300

    ReadNotes function

--- domain/Note.java
+++ domain/Note.java
@@ -55,8 +55,16 @@ public class Note {
         return updatedAt;
     }
 
+    public void setUpdatedAt(LocalDateTime updatedAt){
+        this.updatedAt = updatedAt;
+    }
+
     public LocalDateTime getCreatedAt(){
         return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
     }
 
     public List<String> getTags(){
--- service/NoteService.java
+++ service/NoteService.java
@@ -10,6 +10,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NoteService {
 
@@ -54,27 +56,62 @@ public class NoteService {
         }              
     }
 
+    private List<String> processTags(String tagsLine){
+        if(tagsLine == null || tagsLine.isBlank()){
+            return new ArrayList<>();
+        }
+
+        String[] parts = tagsLine.split(";");
+
+        List<String> tags = new ArrayList<>();
+
+        for(String tag:parts){
+            String cleanTag = tag.trim();
+            if(!cleanTag.isEmpty()){
+                tags.add(cleanTag);
+            }
+        }
+        return tags;
+    }
+
     public Note readNote(Path folderPath, String title){
         String normalizedTitle = NameNormalizer.normalize(title);
 
         Path notePath = folderPath.resolve(normalizedTitle + ".txt");
 
-        //Exception
-        if (noteExists(folderPath, normalizedTitle)){
-            return null;
+        //Warning!
+        if (!noteExists(folderPath, normalizedTitle)){
+            return null; //Exception
         };
 
         try(BufferedReader br = new BufferedReader(new FileReader(notePath.toFile()))){
             Note note = new Note();
+            String line = "";
+            List<String> content = new ArrayList<>();
 
-            String line;
+            note.setTitle(br.readLine());
+
+            note.setCreatedAt(DateFormatter.parse(br.readLine()));
+            note.setUpdatedAt(DateFormatter.parse(br.readLine()));
+
+            String tagsLine = br.readLine();
+            note.setTags(processTags(tagsLine));
+
+            while ((line = br.readLine()) != null) {
+              line = br.readLine();
+              if(line != null && !line.startsWith("#")){
+                content.add(line);
+              }
+            }
+
+            note.setContent(content);  
+
             return note;
         }catch(Exception e){
             e.printStackTrace();
         }
 
         return null;
-
     }
 
     // public void overWriteNote(Path path, Note note){
