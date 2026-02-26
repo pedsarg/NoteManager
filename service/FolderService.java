34549ab6482cd1006a025f54c0a61446e526d3b9
pedsarg <pedsar@gmail.com>
2026-02-26 06:48:34 -0300

    start SearchService implementation

--- service/FolderService.java
+++ service/FolderService.java
@@ -1,16 +1,92 @@
 package Notes.service;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import Notes.util.NameNormalizer;
+
 public class FolderService {
 
     /*
         FolderService:
-            Criar pasta
-            Deletar pasta
-            Renomear pasta
-            Listar
             Mover    
             Copiar
             Existe?
     */
 
+    public void createFolder(Path rootPath, String folderName){
+        String normalizedFolderName = NameNormalizer.normalize(folderName);
+        Path folderPath = rootPath.resolve(normalizedFolderName);
+
+        try{
+            Files.createDirectories(folderPath);
+        }catch(Exception e){
+            System.out.println("Error creating folder: " + e.getMessage());
+        }
+    }
+
+    public void deleteFolder(Path folderPath){
+        
+        if(!Files.exists(folderPath)){
+            throw new RuntimeException("Folder does not exist.");
+        }
+        try{
+            Files.walk(folderPath).sorted(Comparator.reverseOrder()).forEach(path -> {
+                try{
+                    Files.delete(path);
+                }catch(IOException e){
+                    throw new RuntimeException("Error deleting: " + path, e);
+                }
+            });
+        }catch(IOException e){
+            throw new RuntimeException("Error walking through folder.", e);
+        }
+    }
+
+    public void renameFolder(Path folderPath, String oldFolderName,String newFolderName){
+        if(!Files.exists(folderPath)){
+            throw new RuntimeException("Folder does not exist.");
+        }
+
+        String normalizedOldFolderName = NameNormalizer.normalize(oldFolderName);
+        String normalizedFolderName = NameNormalizer.normalize(newFolderName);
+        
+        Path oldFolderPath = folderPath.getParent().resolve(normalizedOldFolderName);
+        Path newFolderPath = folderPath.getParent().resolve(normalizedFolderName);
+
+        if(!Files.exists(oldFolderPath)){
+            throw new RuntimeException("Folder with the old name does not exist.");
+        }
+
+        if(Files.exists(newFolderPath)){
+            throw new RuntimeException("Folder with the new name already exists.");
+        }
+
+        try{
+            Files.move(oldFolderPath, newFolderPath);
+        }catch(IOException e){
+            throw new RuntimeException("Error renaming folder.", e);
+        }
+    }
+
+    public List<String> listFolders(Path rootPath){
+        List<String> folders = new ArrayList<>();
+        
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(rootPath)){
+            for(Path path : stream){
+                if(Files.isDirectory(path)){
+                    folders.add(path.getFileName().toString());
+                }
+            }
+        }catch(IOException e){
+            throw new RuntimeException("Error listing folders.", e);
+        }
+
+        return folders;
+    }
 }
--- service/SearchService.java
+++ service/SearchService.java
@@ -7,8 +7,8 @@ public class SearchService {
             Buscar por titulo
             Buscar por conteudo
             Buscar por tag
-            Buscar por data 
-    
+            Buscar por data de criacao
+            Buscar por data de atualizacao 
     */
 
 }
